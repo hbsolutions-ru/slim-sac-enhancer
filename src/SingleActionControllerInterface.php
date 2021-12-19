5e137bfedff071ce77e1e51e95a2c4e519d4c337
HBS Development Team <dev.hbsolutions@gmail.com>
2021-12-19 22:21:23 +0700

    [MINOR] Init the library

---
+++ src/SingleActionControllerInterface.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace HBS\SacEnhancer;
+
+use Psr\Http\Message\{
+    ResponseInterface as Response,
+    ServerRequestInterface as Request,
+};
+
+interface SingleActionControllerInterface
+{
+    public function __invoke(Request $request, Response $response, $args): Response;
+}
