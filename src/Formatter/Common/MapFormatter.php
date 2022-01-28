5fa56a13018c5970c6d48d9c1512cc4fd75a6a6a
HBS Development Team <dev.hbsolutions@gmail.com>
2022-01-29 02:26:34 +0700

    [FEATURE] Formatter: add Map formatter.

---
+++ src/Formatter/Common/MapFormatter.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace HBS\SacEnhancer\Formatter\Common;
+
+use HBS\SacEnhancer\Formatter\FormatterInterface;
+
+class MapFormatter implements FormatterInterface
+{
+    /**
+     * @var callable
+     */
+    protected $mapper;
+
+    public function __construct(callable $mapper)
+    {
+        $this->mapper = $mapper;
+    }
+
+    public function format($response)
+    {
+        return call_user_func($this->mapper, $response);
+    }
+}
