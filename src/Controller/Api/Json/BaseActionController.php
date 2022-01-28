271727a0f31a16422aba74ae7e8a01c4ef407dde
HBS Development Team <dev.hbsolutions@gmail.com>
2022-01-29 01:55:18 +0700

    [FEATURE] Controller: simplify REST API Controller.

--- src/Controller/Api/Json/BaseActionController.php
+++ src/Controller/Api/Json/BaseActionController.php
@@ -12,7 +12,6 @@ use Psr\Log\{
 };
 use DI\Container;
 use HBS\SacEnhancer\{
-    Controller\Api\Json\ErrorResponse\Factory as ErrorResponseFactory,
     Exception\ExceptionInterface,
     Formatter\Common\EmptyFormatter,
     Formatter\Factory as FormatterFactory,
@@ -28,11 +27,6 @@ abstract class BaseActionController extends \HBS\SacEnhancer\Controller\BaseActi
     protected $logger;
 
     /**
-     * @var ErrorResponseFactory
-     */
-    protected $errorResponseFactory;
-
-    /**
      * @var FormatterFactory
      */
     protected $formatterFactory;
@@ -40,7 +34,6 @@ abstract class BaseActionController extends \HBS\SacEnhancer\Controller\BaseActi
     public function __construct(Container $container)
     {
         $this->logger = $container->has(Logger::class) ? $container->get(Logger::class) : new NullLogger();
-        $this->errorResponseFactory = $container->get(ErrorResponseFactory::class);
         $this->formatterFactory = $container->get(FormatterFactory::class);
     }
 
@@ -65,20 +58,8 @@ abstract class BaseActionController extends \HBS\SacEnhancer\Controller\BaseActi
             $response->getBody()->write((string)json_encode($apiResponse));
         }
 
-        return $response;
-    }
-
-    protected function actionWrapper(Request $request, Response $response, array $args): Response
-    {
-        try {
-            $response = $this->action($request, $response, $args)
-                ->withStatus(static::DEFAULT_CODE_SUCCESS);
-        } catch (\Exception $exception) {
-            $this->logger->error($exception->getMessage());
-            $response = $this->errorResponseFactory->get($exception, $request);
-        }
-
         return $response
+            ->withStatus(static::DEFAULT_CODE_SUCCESS)
             ->withHeader('Content-Type', 'application/json');
     }
 
--- src/Controller/BaseActionController.php
+++ src/Controller/BaseActionController.php
@@ -18,7 +18,7 @@ abstract class BaseActionController implements SingleActionControllerInterface
      */
     public function __invoke(Request $request, Response $response, $args): Response
     {
-        return $this->actionWrapper($request, $response, empty($args) ? [] : (array)$args);
+        return $this->action($request, $response, empty($args) ? [] : (array)$args);
     }
 
     /**
@@ -29,16 +29,4 @@ abstract class BaseActionController implements SingleActionControllerInterface
      * @throws \Exception
      */
     abstract protected function action(Request $request, Response $response, array $args): Response;
-
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @param array $args
-     * @return Response
-     * @throws \Exception
-     */
-    protected function actionWrapper(Request $request, Response $response, array $args): Response
-    {
-        return $this->action($request, $response, $args);
-    }
 }
